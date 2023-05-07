907cfe653d7b712bdaedbcb68c04ec0fb676b482
Emmanuel Mamudu <75863443+dockerman2020@users.noreply.github.com>
2023-05-07 06:47:51 +0100

    Removed unused variables

--- versions.tf
+++ versions.tf
@@ -1,31 +1,16 @@
 terraform {
   required_providers {
-    kind = {
-      source  = "justenwalker/kind"
-      version = "0.17.0"
-    }
-
+    
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.18.1"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.9.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.1"
     }
 
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "1.14.0"
     }
-  }
 
+  }
   required_version = ">= 1.0.0"
 }
-
